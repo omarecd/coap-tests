dec196b59d55365e37d1e36b1c8b5cfa07f320f2
omarecd <omarecd@gmail.com>
2025-06-19 14:39:01 +0200

    Now including a simulated device (server)

--- get_test_value.py
+++ get_test_value.py
@@ -3,12 +3,16 @@ import aiocoap
 
 async def main():
     context = await aiocoap.Context.create_client_context()
-    request = aiocoap.Message(code=aiocoap.GET, uri='coap://californium.eclipseprojects.io/test')
+    #request = aiocoap.Message(code=aiocoap.GET, uri='coap://californium.eclipseprojects.io/obs')
+    #request = aiocoap.Message(code=aiocoap.GET, uri='coap://californium.eclipseprojects.io/test')
+    #request = aiocoap.Message(code=aiocoap.GET, uri='coap://californium.eclipseprojects.io/echo/cali.Ali.LAR91X')
+    #request = aiocoap.Message(code=aiocoap.GET, uri='coap://californium.eclipseprojects.io/obs-pumping')
+    request = aiocoap.Message(code=aiocoap.GET, uri='coap://californium.eclipseprojects.io/obs-large')
 
     try:
         response = await context.request(request).response
         print("Response Code:", response.code)
-        print("Value from /obs:")
+        print("Value from /test:")
         print(response.payload.decode('utf-8'))
     except Exception as e:
         print("Error:", e)
